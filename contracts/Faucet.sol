f1ebdb02c7cc31edbdb14ef3297f5961f86e0e75
Colleen Rose <cjrose59@gmail.com>
2022-05-20 18:52:23 -0700

    sample tests

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,21 +1,36 @@
 //SPDX-License-Identifier: Unlicense
-
 pragma solidity ^0.8.0;
 
 contract Faucet {
-    function withdraw(uint256 _amount) public {
-        // user can withdraw .1 ETH at a time
-        require(
-            _amount <= 100000000000000000,
-            "We can not give you that much test ETH. Please request .1 or less."
-        );
-        require(
-            msg.sender.balance < 10000000000000000000,
-            "Looks like you already have plenty of test ETH."
-        );
+    address payable public owner;
+
+    constructor() payable {
+        owner = payable(msg.sender);
+    }
+
+    event FallbackCalled(address);
+
+    function withdraw(uint256 _amount) public payable {
+        // users can only withdraw .1 ETH at a time, feel free to change this!
+        require(_amount <= 100000000000000000);
         payable(msg.sender).transfer(_amount);
     }
 
-    // fallback function
-    receive() external payable {}
+    function withdrawAll() public onlyOwner {
+        owner.transfer(address(this).balance);
+    }
+
+    function destroyFaucet() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    //  function will be invoked if msg contains no data
+    fallback() external payable {
+        emit FallbackCalled(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 }
